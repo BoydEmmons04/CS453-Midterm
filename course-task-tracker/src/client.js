ac97684c45ccaf68efb88528ea48fcbe542cc48b
BoydEmmons04 <bemmons04@gmail.com>
2026-07-08 17:55:28 -0500

    completed openAPI.yaml

---
+++ course-task-tracker/src/client.js
@@ -0,0 +1,61 @@
+const serverURL = "http://localhost:3000";
+
+async function run() {
+    // Calling health route
+    let response = await fetch(`${serverURL}/health`);
+    console.log("================== PART 1: Health Check ===================")
+    console.log("Health check response: ", await response.json());
+
+    // Create a task
+    console.log("================== PART 2: Create Task ===================")
+    response = await fetch(`${serverURL}/api/tasks`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: "Complete assignment",
+            course: "CS100",
+            completed: false
+        })
+    });
+
+    createdTask = await response.json();
+    console.log("Created: ", createdTask);
+
+    // List all tasks
+    console.log("================== PART 3: List All Tasks ===================")
+    response = await fetch(`${serverURL}/api/tasks`);
+    console.log("All tasks: ", await response.json());
+
+    // Get one task by ID
+    console.log("================== PART 4: Get Single Task ===================")
+    response = await fetch(`${serverURL}/api/tasks/${createdTask.id}`);
+    console.log("Get single task: ", await response.json());
+
+    // Update the task
+    console.log("================== PART 5: Update Task ===================")
+    response = await fetch(`${serverURL}/api/tasks/${createdTask.id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: "Complete assignment",
+            course: "CS453",
+            completed: true
+        })
+    });
+
+    console.log("Updated: ", await response.json());
+
+    // Delete the task
+    console.log("================== PART 6: Delete Task ===================")
+    response = await fetch(`${serverURL}/api/tasks/${createdTask.id}`, {
+        method: "DELETE"
+    });
+    
+    console.log("Deleted: ", response.status);
+}
+
+run();
